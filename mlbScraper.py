789054612044c215d61d23deaa0479ab90c73783
Chris Quinn <cmquinn976@gmail.com>
2016-12-21 14:00:21 -0700

    now scrapes baseball america spreadsheet for teams and positions(only works for players on BA list)

--- mlbScraper.py
+++ mlbScraper.py
@@ -6,6 +6,7 @@ import openSheet
 #This program Scrapes a website for the top fantasy prospects
 
 sheet = openSheet.wks.worksheet("MLB")
+sheet2 = openSheet.wks.worksheet("BaseballAmerica")
 chrome_path = r'C:/Users/Chris/Desktop/chromedriver.exe'
 
 #Opens the site
@@ -16,7 +17,7 @@ driver.get("http://m.mlb.com/prospects/2016")
 content = driver.find_elements_by_class_name('player-name')
 
 
-#updats google sheet with scraped info
+#updates google sheet with scraped info
 i=1
 print("Filling out sheet...")
 for players in content:
@@ -28,6 +29,38 @@ for players in content:
     else:
         first, last = players.text.split(" ")
         sheet.update_acell('A'+str(i), first.lower().capitalize() + " " + last.lower().capitalize())
+
+#Scrapes the baseball america list to scrape the positions as it is easier than doing it from the website
+sheetCell = 1
+sheetList = sheet.range("A2:A101")
+sheet2List = sheet2.range("A2:A101")
+for player in sheetList:
+    sheetCell += 1
+    try:
+        match = sheet2.find(" " + player.value)
+        matchRow = match.row
+        print(match.value)
+        sheet.update_acell('B' + str(sheetCell), sheet2.acell('B' + str(matchRow)).value)
+    except:
+        print("That player was not on Baseball Americas list")
+        pass
+
+#Scrapes the baseball america list to scrape the teams as it is easier than doing it from the website
+sheetCell = 1
+sheetList = sheet.range("A2:A101")
+sheet2List = sheet2.range("A2:A101")
+for player in sheetList:
+    sheetCell += 1
+    try:
+        match = sheet2.find(" " + player.value)
+        matchRow = match.row
+        print(match.value)
+        sheet.update_acell('C' + str(sheetCell), sheet2.acell('C' + str(matchRow)).value)
+    except:
+        print("That player was not on Baseball Americas list")
+        pass
+
+
 
 
 
