a3992dc68304a81df828c103739bee725f58674b
Chris Quinn <cmquinn976@gmail.com>
2016-12-21 14:21:40 -0700

    Made BAScraper its own file, and made both website scrapers functions to call from main

--- main.py
+++ main.py
@@ -1,42 +1,4 @@
-import requests, bs4
-import openSheet
-sheet = openSheet.wks.worksheet("BaseballAmerica")
-#This program Scrapes a website for the top fantasy prospects
+import mlbScraper, BAScraper
 
-#Opens the site (computer language)
-site = requests.get('http://www.baseballamerica.com/minors/midseason-top-100-prospects/#ek0SY6JFtSUcwTlb.97')
-site.raise_for_status()
-
-#opens the site (human language)
-siteSoup = bs4.BeautifulSoup(site.text, 'html.parser')
-
-#singles out the players on the site
-sitePlayers = siteSoup.select('tbody tr')
-
-#Formats the header for each column
-i=0
-for players in siteSoup.findAll('td')[:5]:
-    i+=1
-    sheet.update_cell(1,i, players.getText())
-
-#Prints players and info in appropriate cells
-i=0
-x=2
-for players in siteSoup.findAll('td')[5:]:
-    i+=1
-    sheet.update_cell(x,i, players.getText())
-    if i%5 == 0:
-        x+=1
-        i=0
-
-#Removes the numbers from the player row
-sheetList = sheet.range("A2:A101")
-i =1
-for player in sheetList:
-    i+=1
-    play = player.value[3:]
-    sheet.update_acell('A' + str(i), play)
-
-
-
-print('Your worksheet is filled out')
+BAScraper.scrapeBA()
+mlbScraper.scrapeMLB()
--- mlbScraper.py
+++ mlbScraper.py
@@ -5,63 +5,60 @@ import openSheet
 
 #This program Scrapes a website for the top fantasy prospects
 
-sheet = openSheet.wks.worksheet("MLB")
-sheet2 = openSheet.wks.worksheet("BaseballAmerica")
-chrome_path = r'C:/Users/Chris/Desktop/chromedriver.exe'
+def scrapeMLB():
+    sheet = openSheet.wks.worksheet("MLB")
+    sheet2 = openSheet.wks.worksheet("BaseballAmerica")
+    chrome_path = r'C:/Users/Chris/Desktop/chromedriver.exe'
 
-#Opens the site
-driver = webdriver.Chrome(chrome_path)
-driver.get("http://m.mlb.com/prospects/2016")
+    # Opens the site
+    driver = webdriver.Chrome(chrome_path)
+    driver.get("http://m.mlb.com/prospects/2016")
 
-#Singles out the players names
-content = driver.find_elements_by_class_name('player-name')
+    # Singles out the players names
+    content = driver.find_elements_by_class_name('player-name')
 
+    # updates google sheet with scraped info
+    i = 1
+    print("Filling out sheet...")
+    for players in content:
+        i += 1
+        # Checks if player has 2 or 3 names, and updates google sheet
+        if players.text.count(" ") == 2:
+            first, middle, last = players.text.split(" ")
+            sheet.update_acell('A' + str(i),
+                               first.lower().capitalize() + " " + middle.lower().capitalize() + " " + last.lower().capitalize())
+        else:
+            first, last = players.text.split(" ")
+            sheet.update_acell('A' + str(i), first.lower().capitalize() + " " + last.lower().capitalize())
 
-#updates google sheet with scraped info
-i=1
-print("Filling out sheet...")
-for players in content:
-    i+=1
-    #Checks if player has 2 or 3 names, and updates google sheet
-    if players.text.count(" ") == 2:
-        first, middle, last = players.text.split(" ")
-        sheet.update_acell('A' + str(i), first.lower().capitalize() + " " +middle.lower().capitalize() + " " + last.lower().capitalize())
-    else:
-        first, last = players.text.split(" ")
-        sheet.update_acell('A'+str(i), first.lower().capitalize() + " " + last.lower().capitalize())
+    # Scrapes the baseball america list to scrape the positions as it is easier than doing it from the website
+    sheetCell = 1
+    sheetList = sheet.range("A2:A101")
+    sheet2List = sheet2.range("A2:A101")
+    for player in sheetList:
+        sheetCell += 1
+        try:
+            match = sheet2.find(" " + player.value)
+            matchRow = match.row
+            print(match.value)
+            sheet.update_acell('B' + str(sheetCell), sheet2.acell('B' + str(matchRow)).value)
+        except:
+            print("That player was not on Baseball Americas list")
+            pass
 
-#Scrapes the baseball america list to scrape the positions as it is easier than doing it from the website
-sheetCell = 1
-sheetList = sheet.range("A2:A101")
-sheet2List = sheet2.range("A2:A101")
-for player in sheetList:
-    sheetCell += 1
-    try:
-        match = sheet2.find(" " + player.value)
-        matchRow = match.row
-        print(match.value)
-        sheet.update_acell('B' + str(sheetCell), sheet2.acell('B' + str(matchRow)).value)
-    except:
-        print("That player was not on Baseball Americas list")
-        pass
+    # Scrapes the baseball america list to scrape the teams as it is easier than doing it from the website
+    sheetCell = 1
+    sheetList = sheet.range("A2:A101")
+    sheet2List = sheet2.range("A2:A101")
+    for player in sheetList:
+        sheetCell += 1
+        try:
+            match = sheet2.find(" " + player.value)
+            matchRow = match.row
+            print(match.value)
+            sheet.update_acell('C' + str(sheetCell), sheet2.acell('C' + str(matchRow)).value)
+        except:
+            print("That player was not on Baseball Americas list")
+            pass
 
-#Scrapes the baseball america list to scrape the teams as it is easier than doing it from the website
-sheetCell = 1
-sheetList = sheet.range("A2:A101")
-sheet2List = sheet2.range("A2:A101")
-for player in sheetList:
-    sheetCell += 1
-    try:
-        match = sheet2.find(" " + player.value)
-        matchRow = match.row
-        print(match.value)
-        sheet.update_acell('C' + str(sheetCell), sheet2.acell('C' + str(matchRow)).value)
-    except:
-        print("That player was not on Baseball Americas list")
-        pass
-
-
-
-
-
-print('Your worksheet is filled out')
+    print('Your worksheet is filled out')
